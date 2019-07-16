3d162457d3841977fbacbc318471c6957cb01a9c
Kevin Chau <kevinchau360@gmail.com>
2019-07-15 21:08:30 -0500

    corrected validation, time to test again

--- main.py
+++ main.py
@@ -19,24 +19,7 @@ def index():
    
     #template = jinja_env.get_template('home_login.html')
     return render_template('home_login.html', username_error='', password_error='', validation_error='')
-
-def is_filled(filled):
-    phrase = str(filled).strip
-    try:
-        if len(str(phrase)) > 0:
-            return True
-    except ValueError:
-        return False
-
-def validate_password(password, validation):
-    is_password = is_filled(password)
-    is_validation = is_filled(validation)
-
-    if is_password == is_validation:
-        return True
-    else:
-        return False
-   
+  
 
 @app.route("/", methods=['POST'])
 def validate():
@@ -52,8 +35,8 @@ def validate():
     
     def Is_email(emailaddress): 
         email = str(emailaddress)
-        length = len(emailaddress.strip)
-        if length < 23 and length > 3 and email.count('@') == 1 and email.count('.') == 1:
+        length = len(email.strip())
+        if length <= 23 and length >= 3 and email.count('@') == 1 and email.count('.') == 1:
             return True
         else:
             return False
@@ -65,7 +48,7 @@ def validate():
 
     def is_valid(user_input):
         length = len(str(user_input))
-        if length > 3:
+        if length >= 3 and length<=20:
             return True
         else:
             return False
@@ -78,26 +61,42 @@ def validate():
         test += 1
     else:
         password_error1 = 'Password not valid'
+        validation_error1 = 'Passwords don\'t match'
+        test = 0
     if is_valid(password_confirm):
         test += 1
-        if str(user_password) == str(password_confirm):
-            test += 1
-        else: 
-            validation_error1 = 'Passwords don\'t match'
-    if len(str(email)) < 0:
-        if Is_email(email):
+    if str(user_password) == str(password_confirm):
+        test += 1
+        if is_valid(user_password) == False or is_valid(password_confirm) == False:
+            password_error1 = 'Password not valid'
+            test = 0  
+    else:
+        password_error1 = 'Password not valid'
+        validation_error1 = 'Passwords don\'t match'
+        test = 0
+
+    if len(str(email)) > 0:
+        if Is_email(str(email)):
             test += 1
         else: 
             email_error = 'Email not valid'
-    else:
-        test += 1    
-    
+            test = 0
+    else: 
+        if len(str(email)) == 0:
+            test += 1
+        else:
+            email_error = 'Email not valid'
+            test = 0
+        
+        
+            
+
     if test == 5:
         return redirect('/greeting?username={0}'.format(username))
     else: 
         return render_template('home_login.html', email_error= email_error, username = username,username_error = username_error1, validation_error= validation_error1, password_error = password_error1, user_password = '', password_validation = '')
-    
-    
+
+
     
     
     #return form
